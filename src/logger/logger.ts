a7b566bb7d584c64bccb5217f48fa6bdb2b2c19e
Arghya Chanda <arghyac35@gmail.com>
2022-08-08 20:38:22 +0530

    logger: Reformtat and print logs on console too
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>

--- src/logger/logger.ts
+++ src/logger/logger.ts
@@ -7,14 +7,22 @@ const getLabel = function (module: any) {
   return parts[parts.length - 2] + path.sep + parts.pop();
 };
 
-const customFormat = printf((data: { message: { [x: string]: any; }; level: any; timestamp: any; label: any; }) => {
-  if (data.message && typeof data.message === "object") {
-    return `${data.level}: ${data.timestamp} [${data.label} - ${data.message["fn"]
+const customFormat = printf(
+  (data: {
+    message: { [x: string]: any };
+    level: any;
+    timestamp: any;
+    label: any;
+  }) => {
+    if (data.message && typeof data.message === "object") {
+      return `${data.level}: ${data.timestamp} [${data.label} - ${
+        data.message["fn"]
       }()] ${JSON.stringify(data.message["text"])}`;
-  } else {
-    return `${data.level}: ${data.timestamp} [${data.label}] ${data.message}`;
+    } else {
+      return `${data.level}: ${data.timestamp} [${data.label}] ${data.message}`;
+    }
   }
-});
+);
 
 const getLogger = function (module: any) {
   return new winston.createLogger({
@@ -23,7 +31,15 @@ const getLogger = function (module: any) {
       timestamp(),
       customFormat
     ),
-    transports: [new winston.transports.File({ filename: "combined.log" })],
+    transports: [
+      new winston.transports.Console({
+        format: winston.format.combine(
+          winston.format.cli(),
+          winston.format.splat()
+        ),
+      }),
+      new winston.transports.File({ filename: "combined.log" }),
+    ],
   });
 };
 
