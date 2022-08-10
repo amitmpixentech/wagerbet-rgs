f0514ddf0d67a8c0ae5f3a5fc1cd0110e0308f3c
Arghya Chanda <arghyac35@gmail.com>
2022-08-10 22:13:01 +0530

    server: User port from config
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>

--- src/server.ts
+++ src/server.ts
@@ -1,49 +1,51 @@
-import express, { Application } from "express";
-import compression from "compression";
-import cors from "cors";
-import _ from "underscore";
-import mongo from "./_helper/mongo"
+import express, { Application } from 'express';
+import compression from 'compression';
+import cors from 'cors';
+import _ from 'underscore';
+import mongo from './_helper/mongo';
 import appDataSourceLoader from './orm/dbCreateConnection';
-import gameProviderControllerMappingConfig from "./config/gameProviderControllerMapping.json";
+import gameProviderControllerMappingConfig from './config/gameProviderControllerMapping.json';
 
 import logger from './logger/logger';
-const log = logger(module)
+import config from './config';
+const log = logger(module);
 
 class Server {
-    public app: Application;
+  public app: Application;
 
-    constructor() {
-        this.app = express();
+  constructor() {
+    this.app = express();
 
-        this.configureWebServer();
-        this.configureRoutes();
-        this.configureDB();
+    this.configureWebServer();
+    this.configureRoutes();
+    this.configureDB();
+  }
+
+  private configureWebServer() {
+    this.app.set('port', config.port);
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: false }));
+    this.app.use(compression());
+    this.app.use(cors());
+  }
+
+  private configureRoutes() {
+    for (let gameProvider of _.keys(gameProviderControllerMappingConfig.mappings)) {
+      let key = gameProvider as keyof typeof gameProviderControllerMappingConfig.mappings;
+      this.app.use('/rgs-api/' + gameProvider, require(gameProviderControllerMappingConfig.mappings[key]));
     }
+  }
 
-    private configureWebServer() {
-        this.app.set("port", process.env.PORT || 8080);
-        this.app.use(express.json());
-        this.app.use(express.urlencoded({ extended: false }));
-        this.app.use(compression());
-        this.app.use(cors());
-    }
+  private configureDB() {
+    mongo.init();
+    appDataSourceLoader();
+  }
 
-    private configureRoutes() {
-        for (let gameProvider of _.keys(gameProviderControllerMappingConfig.mappings)) {
-            let key = gameProvider as keyof typeof gameProviderControllerMappingConfig.mappings
-            this.app.use("/rgs-api/" + gameProvider, require(gameProviderControllerMappingConfig.mappings[key]));
-        }
-    }
-
-    private configureDB() { mongo.init();  appDataSourceLoader();}
-
-    public start() {
-        this.app.listen(this.app.get("port"), () => {
-            log.info(
-                `Platform is running at http:localhost:${this.app.get("port")}`
-            );
-        });
-    }
+  public start() {
+    this.app.listen(this.app.get('port'), () => {
+      log.info(`Platform is running at http:localhost:${this.app.get('port')}`);
+    });
+  }
 }
 
 export default Server;
