3cc6622e05656264f5ae3cf3d069989d588765ed
Arghya Chanda <arghyac35@gmail.com>
2022-08-17 22:43:57 +0530

    Add winston daily log rotate
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>

--- src/logger/logger.ts
+++ src/logger/logger.ts
@@ -2,7 +2,14 @@ import config from '../config';
 
 import winston from 'winston';
 import path from 'path';
+import { existsSync, mkdirSync } from 'fs';
 const { combine, timestamp, label, printf } = winston.format;
+
+const logDir = 'logs';
+// Create the log directory if it does not exist
+if (!existsSync(logDir)) {
+  mkdirSync(logDir);
+}
 
 const getLabel = function (module: any) {
   const parts = module.filename.split(path.sep);
@@ -28,7 +35,14 @@ const getLogger = function (module: any) {
       new winston.transports.Console({
         format: winston.format.combine(winston.format.cli(), winston.format.splat()),
       }),
-      new winston.transports.File({ filename: 'combined.log' }),
+      new (require('winston-daily-rotate-file'))({
+        filename: `${logDir}/application-%DATE%.log`,
+        datePattern: 'YYYY-MM-DD',
+        auditFile: `${logDir}/audit.json`,
+        level: config.logs.level,
+        maxSize: '20m',
+        maxFiles: '14d',
+      }),
     ],
   });
 };
